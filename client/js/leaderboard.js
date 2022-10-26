bd48816092e89a1e381f2dc0b4e51f82cfede47d
ahmed <ahmedmuhammed78613@gmail.com>
2022-10-26 16:09:52 +0100

    leaderboards page added & remove habit working

---
+++ client/js/leaderboard.js
@@ -0,0 +1,15 @@
+// Buttons
+const toggleBtn = document.getElementById("toggle");
+
+// Event Listeners
+toggleBtn.addEventListener("click", toggleLeaderboards);
+
+// Functions
+async function toggleLeaderboards(e) {
+	const currentMode = e.target.getAttribute("toggle-value");
+	if (currentMode) {
+		// TODO
+	} else {
+		// TODO
+	}
+}
