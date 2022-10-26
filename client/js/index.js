59cb1ac5f6b1e49e78e2ef8e5dd5f4daef8e6554
ahmed <ahmedmuhammed78613@gmail.com>
2022-10-26 17:00:32 +0100

    updated leaderboard endpoint

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,6 @@
 // Global Variables
 const frontendURL = window.location.origin;
 const backendURL = "https://monasteri.herokuapp.com";
-
 // Forms
 const loginForm = document.getElementById("login-form");
 const registerForm = document.getElementById("register-form");
--- client/js/leaderboard.js
+++ client/js/leaderboard.js
@@ -1,15 +1,63 @@
+// Global Variables
+const frontendURL = window.location.origin;
+const backendURL = "https://monasteri.herokuapp.com";
 // Buttons
 const toggleBtn = document.getElementById("toggle");
+const goBackBtn = document.getElementById("go-back-btn");
 
 // Event Listeners
 toggleBtn.addEventListener("click", toggleLeaderboards);
+goBackBtn.addEventListener(
+	"click",
+	() => (window.location = `${frontendURL}/client/habitsNew.html`)
+);
 
 // Functions
 async function toggleLeaderboards(e) {
-	const currentMode = e.target.getAttribute("toggle-value");
-	if (currentMode) {
+	// Get Dat Attribute
+	const currentMode = e.target.getAttribute("toggle-score");
+	// Toggle Dat Attribute
+	if (currentMode === "totalStreak") {
+		e.target.setAttribute("toggle-score", "totalHabits");
+	} else e.target.setAttribute("toggle-score", "totalStreak");
+	const currentModeUpdated = e.target.getAttribute("toggle-score");
+	// Get API Request
+	console.log(currentModeUpdated);
+	try {
 		// TODO
-	} else {
-		// TODO
+		const res = await fetch(`${backendURL}/habits/Leaderboard/`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify({
+				rankBy: currentModeUpdated,
+			}),
+		});
+		const data = await res.json();
+		console.log(data);
+	} catch (error) {}
+}
+
+async function loadScoreData() {
+	// Get Local Storage Data
+	const token = localStorage.getItem("session");
+	const currentMode = toggleBtn.getAttribute("toggle-score");
+
+	try {
+		const res = await fetch(`${backendURL}/habits/leaderboard/totalStreak`, {
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const data = await res.json();
+		console.log(data);
+		console.log(data);
+	} catch (error) {
+		console.log(error);
 	}
 }
+
+loadScoreData();
