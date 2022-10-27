6570f0b2bb950bb50ad53c9bf0a7d98ad1fc7ef1
David <siddav83@gmail.com>
2022-10-27 11:11:17 +0100

    init testing

---
+++ server/tests/intergration/product.test.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require("../../index");
+
+require("dotenv").config();
+
+/* Connecting to the database before each test. */
+beforeEach(async () => {
+    await mongoose.connect(process.env.DATABASE_URL);
+  });
+  
+  /* Closing database connection after each test. */
+  afterEach(async () => {
+    await mongoose.connection.close();
+  });
+
+  describe("GET habits/user", () => {
+    it("should return all users", async () => {
+      const res = await request(app).get("/");
+      expect(res.statusCode).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+  });
