e470424bfd355ef286a6e309d55fdf3ccb7d7cf3
David <siddav83@gmail.com>
2022-10-27 12:17:14 +0100

    3 test passing

--- server/tests/intergration/product.test.js
+++ server/tests/intergration/product.test.js
@@ -3,13 +3,15 @@ const request = require("supertest");
 const app = require("../../index");
 const jwt = require("jsonwebtoken");
 
+
 require("dotenv").config();
 
 const token = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWRtaW4iLCJpYXQiOjE2NjY3ODEyMTV9.mURhBMZXogpiq2MaVhBF25DksH2JW3KGfJKwXN_ZN_M`;
 
 /* Connecting to the database before each test. */
-beforeEach(async () => {
-    await mongoose.connect(process.env.DATABASE_URL);
+beforeAll(async () => {
+    await mongoose.connection.close();
+    await mongoose.connect(process.env.TESTING_DATABASE_URL);
   });
 
 //   async function getAdminToken() {
@@ -30,7 +32,7 @@ beforeEach(async () => {
 // }
   
   /* Closing database connection after each test. */
-  afterEach(async () => {
+  afterAll(async () => {
     await mongoose.connection.close();
   });
 
@@ -41,18 +43,21 @@ beforeEach(async () => {
     });
   });
 
-  describe("GET /users ", () => {
+  describe("GET /habits ", () => {
     it("should return all users", async () => {
-      const res = await request(app).get("/user");
+      const res = await request(app).get("/habits/user").set('Authorization', token);
       expect(res.statusCode).toBe(200);
     //   expect(res.body.length).toBeGreaterThan(0);
     });
   });
 
-  describe("GET /habits ", () => {
-    it("should return all users", async () => {
-      const res = await request(app).get("/habits/user");
-      expect(res.statusCode).toBe(200);
-    //   expect(res.body.length).toBeGreaterThan(0);
-    });
-  });
+//   describe("POST /users", () => {
+//     it("should create a user account", async () => {
+//       const res = await request(app).post("/users/").send({
+//         username: "peterdgfdg",
+//         password: "12345678dfg",
+//       });
+//       expect(res.statusCode).toBe(201);
+//     //   expect(res.body.name).toBe("Product 2");
+//     });
+//   })
