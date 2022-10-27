80a28948d96cbe8962f57bca80a1683c8d35f002
David <siddav83@gmail.com>
2022-10-27 11:50:34 +0100

    added token for testing

--- server/tests/intergration/product.test.js
+++ server/tests/intergration/product.test.js
@@ -1,23 +1,58 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
 const app = require("../../index");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
+
+const token = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWRtaW4iLCJpYXQiOjE2NjY3ODEyMTV9.mURhBMZXogpiq2MaVhBF25DksH2JW3KGfJKwXN_ZN_M`;
 
 /* Connecting to the database before each test. */
 beforeEach(async () => {
     await mongoose.connect(process.env.DATABASE_URL);
   });
+
+//   async function getAdminToken() {
+//     console.log("getAdminToken: Start");
+//     loginToken = "";
+//     const response = await 
+//                         myApp.
+//                         post('/users/login').
+//                         send({
+//                             "username": "admin",
+//                             "password": "admin"
+//                         });
+
+//     loginToken = response.body.Data.token;
+
+//     console.log("getAdminToken: End");
+//     return loginToken;
+// }
   
   /* Closing database connection after each test. */
   afterEach(async () => {
     await mongoose.connection.close();
   });
 
-  describe("GET habits/user", () => {
+  describe("GET / ", () => {
     it("should return all users", async () => {
-      const res = await request(app).get("/");
+      const res = await request(app).get("/").set('Authorization', token);
       expect(res.statusCode).toBe(200);
-      expect(res.body.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("GET /users ", () => {
+    it("should return all users", async () => {
+      const res = await request(app).get("/user");
+      expect(res.statusCode).toBe(200);
+    //   expect(res.body.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("GET /habits ", () => {
+    it("should return all users", async () => {
+      const res = await request(app).get("/habits/user");
+      expect(res.statusCode).toBe(200);
+    //   expect(res.body.length).toBeGreaterThan(0);
     });
   });
